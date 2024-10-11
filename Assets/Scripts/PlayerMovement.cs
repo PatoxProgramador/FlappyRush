244435f31042d57ab705a1bf10f1323fc886b36c
Dumbita <magalan942@gmail.com>
2024-10-11 19:21:58 -0300

    jump boost
    jump boost in ducks script
    
    but the prototype tutorial script still not there, waiting for approval and testing

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,7 +6,7 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
 
-    [SerializeField] private float velocity = 3f;// jumping up force/speed
+    [SerializeField] private float velocity = 5f;// jumping up force/speed
     [SerializeField] private float speed = 1f;// movement speed
 
     public Sprite[] faces;
@@ -31,17 +31,44 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
         {
 
-            body.velocity = Vector2.up * velocity;
+            //body.velocity = Vector2.up * velocity;
+            StartCoroutine(changeJumpBoost());
 
         }
         //movement setup
         horizontal = Input.GetAxisRaw("Horizontal");
+
     }
 
     private void FixedUpdate()
     {
         // movement action
         body.velocity = new Vector2(horizontal * speed, body.velocity.y);
+
+    }
+    //jump friction -- velocity decreases over time
+    IEnumerator changeJumpBoost()
+    {
+
+        velocity -= 0.4f;
+        body.velocity = Vector2.up * velocity;
+
+        yield return new WaitForSeconds(0.05f);
+
+        if (velocity > 0)
+        {
+
+            StartCoroutine(changeJumpBoost());
+
+        }
+        else
+        {
+
+            StopCoroutine(changeJumpBoost());
+            velocity = 5;
+
+        }
+
 
     }
 }
