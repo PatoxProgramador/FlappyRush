1c0df5864e035f7f98f53e300e90bd2ac60b3156
Dumbita <magalan942@gmail.com>
2024-10-12 16:51:28 -0300

    portal effects

--- Assets/Scripts/Testing/PortalController.cs
+++ Assets/Scripts/Testing/PortalController.cs
@@ -9,9 +9,11 @@ public class PortalController : MonoBehaviour
     Animation anim;
     Rigidbody2D playerRb;
     private void Awake() {
+
         player = GameObject.FindGameObjectWithTag("Player");
         anim = player.GetComponent<Animation>();  
         playerRb = player.GetComponent<Rigidbody2D>();
+
     } 
     private void OnTriggerEnter2D(Collider2D collision) 
     {
@@ -25,26 +27,36 @@ public class PortalController : MonoBehaviour
     }
     IEnumerator PortalIn()
     {
+
         playerRb.simulated = false;
         anim.Play("portal In");
         StartCoroutine(MoveInPortal());
         yield return new WaitForSeconds(0.5f);
+
         player.transform.position = destination.transform.position;
         playerRb.velocity = Vector2.zero;
         anim.Play("portal Out");
         yield return new WaitForSeconds(0.5f);
+
         playerRb.simulated = true;
+
     }
 
     IEnumerator MoveInPortal()
     {
+
         float timer = 0;
+
         while (timer < 0.5f)
         {
+
             player.transform.position = Vector2.MoveTowards(player.transform.position, transform.position, 3 * Time.deltaTime);
+
             yield return new WaitForEndOfFrame();
+
             timer += Time.deltaTime;
 
         }
+
     }
 }
