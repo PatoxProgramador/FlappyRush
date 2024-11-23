12b479212b2058fbd58741440f2bc82e0c0ab6c9
yair-we <yairmotiuk@gmail.com>
2024-11-23 12:15:39 +0200

    executive chef done

--- Assets/Scripts/Enemy/Health/ExecutiveChefHealth.cs
+++ Assets/Scripts/Enemy/Health/ExecutiveChefHealth.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class ExecutiveChefHealth : MonoBehaviour
 {
-
+    public GameObject[] week = new GameObject[3];
+    public GameObject[] medium = new GameObject[2];
+    public GameObject hard;
+    public GameObject spawnPoint;
     [SerializeField]private float health;
     public float maxHealth;
 
@@ -13,6 +16,7 @@ public class ExecutiveChefHealth : MonoBehaviour
 
     void Start()
     {
+        spawnPoint = GameObject.FindGameObjectWithTag("spawnPoint");
         //sets health to max
         health = maxHealth;
         
@@ -29,9 +33,44 @@ public class ExecutiveChefHealth : MonoBehaviour
         {
 
             Destroy(gameObject);
+            var randomChance = Random.Range(0, 100);
+            if (randomChance == 0)
+                {
+                    foreach (GameObject obj in week)
+                        {
+                            Instantiate(obj, spawnPoint.transform.position, spawnPoint.transform.rotation);
+                        }
 
+                    foreach (GameObject obj in medium)
+                        {
+                            Instantiate(obj, spawnPoint.transform.position, spawnPoint.transform.rotation);
+                        }
+
+                            Instantiate(hard, spawnPoint.transform.position, spawnPoint.transform.rotation);
+                }
+            else
+            {    var randomChef = Random.Range(0,3);
+            
+                if (randomChef == 0)
+                    {
+                        foreach (GameObject obj in week)
+                        {
+                            Instantiate(obj, spawnPoint.transform.position, spawnPoint.transform.rotation);
+                        }
+                    }
+                if (randomChef == 1)
+                    {
+                        foreach (GameObject obj in medium)
+                        {
+                            Instantiate(obj, spawnPoint.transform.position, spawnPoint.transform.rotation);
+                        }
+                    }
+                if (randomChef == 2)
+                    {
+                        Instantiate (hard, spawnPoint.transform.position, spawnPoint.transform.rotation); 
+                    }
+            }
         }
-
     }
 
     private void OnTriggerEnter2D(Collider2D other) 
