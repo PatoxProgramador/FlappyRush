59b54b33f2dc6c7d8c02f6e41584e4281d6817a4
Dumbita <magalan942@gmail.com>
2024-11-29 12:45:46 -0300

    FirstRoom UI screens and scene connections
    Pause and death screen added
    connection with title screen in build profile
    
    removed some duplicate scripts and unnecessary comments

--- Assets/Scripts/StartScreen/SceneChange.cs
+++ Assets/Scripts/StartScreen/SceneChange.cs
@@ -60,12 +60,6 @@ public class SceneChange : MonoBehaviour
  
         pause.SetActive(true);
 
-        /*
-        script.enabled = true;
-        wait = true;
-        Pause.isPaused = true;
-        */
-
         script.Paused();
 
     }
